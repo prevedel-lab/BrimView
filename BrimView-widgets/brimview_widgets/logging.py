171964e60a8a42b61836b37cd5380f657723b30f
Carlo Bevilacqua <carlo.bevilacqua@embl.de>
2025-10-07 13:03:26 +0200

    First attempt to log into the admin panel

--- BrimView-widgets/brimview_widgets/logging.py
+++ BrimView-widgets/brimview_widgets/logging.py
@@ -1,4 +1,24 @@
-import logging
+# Wether to use the panel logger, whose output is shown in the admin panel 
+_use_panel_logger = False
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+if _use_panel_logger:
+    raise NotImplementedError('Logging with "pn.state.log" raises an error when the admin panel is open')
+    import panel as pn
+    class PanelLogger:
+        def _log(self, msg, level): 
+            pn.state.log(msg, level)
+        def debug (self, msg):
+            self._log(msg, 'debug') 
+        def info (self, msg):
+            self._log(msg, 'info')
+        def warning (self, msg):
+            self._log(msg, 'warning') 
+        def error (self, msg):
+            self._log(msg, 'error')
+        def critical (self, msg):
+            self._log(msg, 'critical') 
+    logger = PanelLogger()
+else:
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
